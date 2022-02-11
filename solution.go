f56f10e4f9394548222110318ee16317d04e1943
eremin.is2 <ivan.eremin@domru.ru>
2022-02-11 23:22:39 +0700

    refactoring

--- solution.go
+++ solution.go
@@ -1,9 +1,15 @@
 package solution
 
 import (
+	"fmt"
+
 	"github.com/kyokomi/emoji"
 )
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
+}
+
+func main() {
+	fmt.Println(GetMessage())
 }
